0a331757cb39bcbfb1cbccda188e197e21c4a981
srlitem <santiagorhenals@gmail.com>
2023-07-31 20:52:02 -0500

    Adding validation about the length of an account name

--- users/serializers.py
+++ users/serializers.py
@@ -7,6 +7,11 @@ class AccountSerializer(serializers.ModelSerializer):
         model = Account
         fields = ['id', 'user', 'name', 'balance']
 
+    def validate_name(self, value):
+        if len(value) > 10:
+            raise serializers.ValidationError("The name can not have more than 10 characters")
+        return value
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
