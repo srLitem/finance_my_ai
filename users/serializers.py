7486e7706a8451357bb3ec296e2c7cdd6895cdba
srlitem <santiagorhenals@gmail.com>
2023-08-01 12:07:25 -0500

    Overriding create method for an account to provide a default one

--- users/serializers.py
+++ users/serializers.py
@@ -6,11 +6,24 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'user', 'name', 'balance']
+        extra_kwargs = {
+            'name': {'required': False}
+        }
 
     def validate_name(self, value):
         if len(value) > 10:
             raise serializers.ValidationError("The name can not have more than 10 characters")
         return value
+
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        if name is None:
+            validated_data['name'] = 'Default Name'
+        account = super().create(validated_data)
+        if name is None:
+            account.name = 'Account_' + str(account.id)
+            account.save()
+        return account
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
